410da871dde73c8b701730c661c43adc2fd5d098
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-01-14 12:47:20 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+
+class Solution {
+public:
+    int PalinArray(int a[], int n)
+    {
+    	// code here
+    	for(int i=0;i<n;i++)
+    	{
+    	    int num=a[i];
+    	    int sum=0;
+    	    while(num!=0)
+    	    {
+    	        sum=sum*10+num%10;
+    	        num=num/10;
+    	    }
+    	    if(sum!=a[i])
+    	    {
+    	        return 0;
+    	    }
+    	}
+    	return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+			cin>>a[i];
+		Solution obj;
+		cout<<obj.PalinArray(a,n)<<endl;
+	}
+}
+// } Driver Code Ends
