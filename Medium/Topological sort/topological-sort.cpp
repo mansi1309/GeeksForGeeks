6d3dd91e1af5017609c745fd6f76e9fb1dcc753d
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-06-01 22:56:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -6,34 +6,66 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to return list containing vertices in Topological order. 
-	void dfs(int i,vector<int> adj[],vector<int>&vis,stack<int>&st){
-	    vis[i]=1;
-	    for(auto &it:adj[i]){
-	        if(vis[it]==0){
-	            dfs(it,adj,vis,st);
-	        }
+// 	//Function to return list containing vertices in Topological order. 
+// 	void dfs(int i,vector<int> adj[],vector<int>&vis,stack<int>&st){
+// 	    vis[i]=1;
+// 	    for(auto &it:adj[i]){
+// 	        if(vis[it]==0){
+// 	            dfs(it,adj,vis,st);
+// 	        }
 	        
-	    }
-	    st.push(i);
-	}
+// 	    }
+// 	    st.push(i);
+// 	}
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    vector<int>vis(V,0);
-	    stack<int>st;
-	    for(int i=0;i<V;i++){
-	        if(vis[i]==0){
-	            dfs(i,adj,vis,st);
-	        }
-	    }
-	    vector<int>res;
-	    while(!st.empty()){
-	        res.push_back(st.top());
-	        st.pop();
-	    }
-	    return res;
-	    }
+	   // by dfs
+	   // vector<int>vis(V,0);
+	   // stack<int>st;
+	   // for(int i=0;i<V;i++){
+	   //     if(vis[i]==0){
+	   //         dfs(i,adj,vis,st);
+	   //     }
+	   // }
+	   // vector<int>res;
+	   // while(!st.empty()){
+	   //     res.push_back(st.top());
+	   //     st.pop();
+	   // }
+	   // return res;
+	   // }
+	   
+	   //by bfs
+	   vector<int>indegree(V,0);
+	   for(int i=0;i<V;i++){
+	       for(auto &it:adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   queue<int>q;
+	   for(int i=0;i<V;i++){
+	       if(indegree[i]==0){
+	           q.push(i);
+	       }
+	   }
+	   vector<int>topo;
+	   while(!q.empty()){
+	       int val=q.front();
+	       q.pop();
+	       topo.push_back(val);
+	       	  for(auto &it:adj[val]){
+	           indegree[it]--;
+	           if(indegree[it]==0){
+	               q.push(it);
+	           }
+	       }
+	     
+	       
+	   }
+	     return topo;
+	}
+	   
 };
 
 //{ Driver Code Starts.
