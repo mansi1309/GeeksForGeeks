c0e3e7afdaa50e0db0f3fa9567996e198c12d700
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-08-11 23:24:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
@@ -26,34 +26,35 @@ struct Job
 class Solution 
 {
     public:
-    //Function to find the maximum profit and the number of jobs done.
-    static bool cmp(Job a,Job b){
-        return a.profit>b.profit;
+    bool static comp(Job a, Job b){
+        return a.profit > b.profit;
     }
+    //Function to find the maximum profit and the number of jobs done.
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         // your code here
-        sort(arr,arr+n,cmp);
-        int maxi=0;
+         int count =0, profit=0;
+        sort(arr, arr+n, comp);
+        int maxi =arr[0].dead;
         for(int i=0;i<n;i++){
             maxi=max(maxi,arr[i].dead);
         }
-        // int slot[maxi+1]={-1};
-          vector<int> slot(maxi + 1, -1);
-        int cntjob=0;
-        int totalProfit=0;
+        int slot[maxi+1];
+        for(int i=0;i<=maxi;i++) slot[i]=-1;
+        
+        int countJobs=0, jobProfit=0;
         
         for(int i=0;i<n;i++){
             for(int j=arr[i].dead;j>0;j--){
                 if(slot[j]==-1){
-                    slot[j]=arr[i].id;
-                    cntjob++;
-                    totalProfit+=arr[i].profit;
+                    slot[j]=i;
+                    countJobs++;
+                    jobProfit += arr[i].profit;
                     break;
                 }
             }
         }
-        return {cntjob,totalProfit};
+        return {countJobs, jobProfit};
     } 
 };
 
