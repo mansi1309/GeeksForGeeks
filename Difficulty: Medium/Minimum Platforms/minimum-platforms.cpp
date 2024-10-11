c1727febbb5998f3d46987f5e0d78e00f7e7a50e
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-10-11 11:11:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -6,56 +6,52 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
-    //Function to find the minimum number of platforms required at the
-    //railway station such that no train waits.
-    int findPlatform(int arr[], int dep[], int n)
-    {
-    	// Your code here
-    	sort(arr,arr+n);
-    	sort(dep,dep+n);
-    	
-    	int i=0;
-    	int j=0;
-    	int cnt=0;
-    	int maxCnt=0;
-    	while(i<n){
-    	    if(arr[i]<=dep[j]){ //arrival
-    	        cnt++;
-    	        i++;
-    	    }
-    	    else{ // deparature
-    	        cnt--;
-    	        j++;
-    	    }
-    	    maxCnt=max(cnt,maxCnt);
-    	}
-    	return maxCnt;
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int maxi=1;
+        int needP=1;
+        int j=0;
+        int i=1;
+        while(i<arr.size()){
+            if(arr[i]>dep[j]){
+                maxi--;
+                j++;
+            }
+            else{
+                maxi++;
+                i++;
+            }
+            needP=max(needP,maxi);
+        }
+        return needP;
     }
 };
 
 
 //{ Driver Code Starts.
-// Driver code
-int main()
-{
+//  Driver code
+int main() {
     int t;
-    cin>>t;
-    while(t--) 
-    {
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        int arr[n];
-        int dep[n];
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        for(int j=0;j<n;j++){
-            cin>>dep[j];
+        cin >> n;
+        vector<int> arr(n);
+        vector<int> dep(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        for (int j = 0; j < n; j++) {
+            cin >> dep[j];
         }
         Solution ob;
-        cout <<ob.findPlatform(arr, dep, n)<<endl;
-    } 
-   return 0;
+        cout << ob.findPlatform(arr, dep) << endl;
+    }
+    return 0;
 }
 // } Driver Code Ends
