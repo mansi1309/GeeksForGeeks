0c5b71917c5e1d2880b498f95200c565ea0fc624
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-12-10 19:02:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -8,26 +8,16 @@ class Solution {
   public:
     // Function to find element in sorted array
     // arr: input array
-    // N: size of array
-    // K: element to be searched
-    int searchInSorted(int arr[], int N, int K) {
+    // k: element to be searched
+    bool searchInSorted(vector<int> arr, int k) {
 
         // Your code here
-        int low=0;
-        int high=N-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(arr[mid]==K){
-                return 1;
-            }
-            else if(arr[mid]<K){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==k){
+                return true;
             }
         }
-        return -1;
+        return false;
     }
 };
 
@@ -37,18 +27,24 @@ int main(void) {
 
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n, k;
-        cin >> n >> k;
 
-        int arr[n];
-
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
 
+        int k;
+        cin >> k;
+        cin.ignore();
         Solution ob;
-        cout << ob.searchInSorted(arr, n, k) << endl;
+        cout << (ob.searchInSorted(arr, k) ? "true" : "false") << endl;
+        cout << "~" << endl;
     }
 
     return 0;
