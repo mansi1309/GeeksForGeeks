c91a7e5ad5f7ad82f40138211a3d31a3a04f2381
Mansi Rai <133951716+mansi1309@users.noreply.github.com>
2024-12-19 01:07:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Sorted Arrays with Duplicate Elements/union-of-two-sorted-arrays-with-duplicate-elements.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        int n=a.size();
+        int m=b.size();
+        vector<int>v(n+m);
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<n && j<m){
+            if(a[i]<b[j]){
+                v[k++]=a[i++];
+            }
+            else{
+                v[k++]=b[j++];
+            }
+        } 
+        while(i<n){
+            v[k++]=a[i++];
+        }
+        while(j<m){
+            v[k++]=b[j++];
+        }
+        set<int>st;
+        for(int i=0;i<v.size();i++){
+            st.insert(v[i]);
+        }
+         vector<int>ans(st.begin(),st.end());
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
